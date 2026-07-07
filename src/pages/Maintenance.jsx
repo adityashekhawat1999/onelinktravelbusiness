02d6d3e767f0328cbf675fdb0d5111b6b46282e8
Aditya Pratap Singh <adityashekhawat1999@gmail.com>
2026-07-07 17:21:42 +0530

    Add Maintenance component for downtime message

--- src/pages/Maintenance.jsx
+++ src/pages/Maintenance.jsx
@@ -1,1 +1,19 @@
+function Maintenance() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        textAlign: "center",
+      }}
+    >
+      <h1>🚧 Under Maintenance</h1>
+      <p>We'll be back shortly.</p>
+    </div>
+  );
+}
 
+export default Maintenance;
